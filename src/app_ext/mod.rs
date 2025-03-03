78035ef764de734984ef4141889ef267cb2bec90
Michael Scofield <mscofield0@tutanota.com>
2025-03-03 02:29:01 +0100

    remove useless methods on AppExt and fix new ones

--- src/app_ext/mod.rs
+++ src/app_ext/mod.rs
@@ -13,7 +13,10 @@ use bevy::{
 	prelude::{Deref, DerefMut},
 };
 
-use crate::par_events::{ParEvents, ParManualEventReader};
+use crate::{
+	par_events::{ParEvents, ParManualEventReader},
+	event_wrapper::Event,
+};
 
 /// A helper trait for enforcing bounds on assert helpers.
 pub trait AssertHelper: Send + Sync + Clone + Debug + PartialEq + 'static {}
@@ -22,32 +25,15 @@ impl<T: Send + Sync + Clone + Debug + PartialEq + 'static> AssertHelper for T {}
 
 /// Extends the `App` trait with additional utility methods.
 pub trait AppExt {
-	/// Observes all events of the specified type.
-	fn observe_events<E: Event + Clone>(&mut self) -> Vec<E>;
-	/// Observes all par events of the specified type.
-	fn observe_par_events<E: Event + Clone>(&mut self) -> Vec<E>;
 	/// Adds a custom schedule after the specified schedule.
 	fn add_schedule_after(&mut self, schedule: impl ScheduleLabel + Clone, after: impl ScheduleLabel);
 	/// Adds systems to a set to the app.
 	fn add_systems_to_set<M>(&mut self, set: impl SystemSet, systems: impl IntoSystemConfigs<M>);
-	/// Asserts that the tick returns the specified [`wire::Res`].
-	fn assert_ok<Event: AssertHelper, Err: AssertHelper>(&mut self, expected: impl Into<Vec<wire::Res<Event>>>);
-	/// Asserts that the tick returns the specified [`wire::Error`].
-	fn assert_err<Event: AssertHelper, Err: AssertHelper>(&mut self, expected: impl Into<Vec<wire::Error<Err>>>);
-	/// Asserts that the tick returns no [`wire::Error`].
-	fn assert_no_err<Event: AssertHelper, Err: AssertHelper>(&mut self);
-	/// Updates a tick and asserts that the tick returns no [`wire::Error`].
-	fn update_no_err<Event: AssertHelper, Err: AssertHelper>(&mut self);
-	/// Inspects the queried state of the world.
-	fn inspect_state<D: bevy::ecs::query::QueryData>(&mut self, f: impl FnMut(<<D as bevy::ecs::query::QueryData>::ReadOnly as WorldQuery>::Item<'_>));
-	/// Inspects the resource of the world.
-	fn inspect_res<R: Resource>(&mut self, f: impl FnMut(&R));
+	/// Returns all events that were queued in the last two ticks.
+	fn events<E: Send + Sync + Clone + 'static>(&self) -> Vec<E>;
 
 	/// Returns all events that were queued in the last two ticks.
-	fn events<E: Event + Clone>(&self) -> Vec<E>;
-
-	/// Returns all events that were queued in the last two ticks.
-	fn par_events<E: Event + Clone>(&self) -> Vec<E>;
+	fn par_events<E: Send + Sync + Clone + 'static>(&self) -> Vec<E>;
 
 	/// Returns the specified resource.
 	fn res<R: Resource + Clone>(&self) -> R;
@@ -63,16 +49,16 @@ pub trait AppExt {
 }
 
 impl AppExt for bevy::app::App {
-	fn events<E: Event + Clone>(&self) -> Vec<E> {
-		let events = self.world().resource::<Events<E>>();
+	fn events<E: Send + Sync + Clone + 'static>(&self) -> Vec<E> {
+		let events = self.world().resource::<Events<Event<E>>>();
 		let mut cursor = events.get_cursor();
-		cursor.read(&events).cloned().collect()
+		cursor.read(&events).cloned().map(Event::into_inner).collect()
 	}
 
-	fn par_events<E: Event + Clone>(&self) -> Vec<E> {
-		let events = self.world().resource::<ParEvents<E>>();
+	fn par_events<E: Send + Sync + Clone + 'static>(&self) -> Vec<E> {
+		let events = self.world().resource::<ParEvents<Event<E>>>();
 		let mut reader = events.get_reader();
-		reader.read(&events).cloned().collect()
+		reader.read(&events).cloned().map(Event::into_inner).collect()
 	}
 
 	fn res<R: Resource + Clone>(&self) -> R {
@@ -100,26 +86,6 @@ impl AppExt for bevy::app::App {
 			.send_event(crate::event_wrapper::Event::new(wire::Req::<A>::new(target.into(), action, corrid)));
 		corrid
 	}
-
-	fn observe_events<E: Event + Clone>(&mut self) -> Vec<E> {
-		let events_res = self.world().resource::<Events<E>>();
-		events_res.get_cursor().read(&events_res).cloned().collect()
-	}
-
-	fn observe_par_events<E: Event + Clone>(&mut self) -> Vec<E> {
-		self.world_mut().init_resource::<ParObserver<E>>();
-
-		let events_res = self.world().resource::<ParEvents<E>>();
-		// SAFETY: Used only in testing purposes where systems are controlled via manual ticks.
-		let mut observer = unsafe {
-			self.world()
-				.as_unsafe_world_cell_readonly()
-				.get_resource_mut::<ParObserver<E>>()
-				.expect("Observer resource not initialized")
-		};
-		observer.read(&events_res).cloned().collect()
-	}
-
 	fn add_schedule_after(&mut self, schedule: impl ScheduleLabel + Clone, after: impl ScheduleLabel) {
 		self.init_schedule(schedule.clone());
 		let mut main_schedule = self.world_mut().resource_mut::<bevy::app::MainScheduleOrder>();
@@ -128,86 +94,5 @@ impl AppExt for bevy::app::App {
 
 	fn add_systems_to_set<M>(&mut self, set: impl SystemSet, systems: impl IntoSystemConfigs<M>) {
 		self.add_systems(bevy::app::Update, systems.in_set(set));
-	}
-
-	fn assert_ok<Event: AssertHelper, Err: AssertHelper>(&mut self, expected: impl Into<Vec<wire::Res<Event>>>) {
-		let expected: Vec<wire::Res<Event>> = expected.into();
-		let got = self
-			.observe_par_events::<crate::event_wrapper::Event<wire::Res<Event>>>()
-			.into_iter()
-			.map(|x| x.into_inner())
-			.collect::<Vec<_>>();
-		let errs = self
-			.observe_par_events::<crate::event_wrapper::Event<wire::Error<Err>>>()
-			.into_iter()
-			.map(|x| x.into_inner())
-			.collect::<Vec<_>>();
-		if !errs.is_empty() {
-			dbg!(&errs);
-			panic!("assertion failed, see above");
-		}
-
-		assert_eq!(got, expected);
-	}
-
-	fn assert_err<Event: AssertHelper, Err: AssertHelper>(&mut self, expected: impl Into<Vec<wire::Error<Err>>>) {
-		let expected: Vec<wire::Error<Err>> = expected.into();
-		let got = self
-			.observe_par_events::<crate::event_wrapper::Event<wire::Error<Err>>>()
-			.into_iter()
-			.map(|x| x.into_inner())
-			.collect::<Vec<_>>();
-		let events = self
-			.observe_par_events::<crate::event_wrapper::Event<wire::Res<Event>>>()
-			.into_iter()
-			.map(|x| x.into_inner())
-			.collect::<Vec<_>>();
-		if !events.is_empty() {
-			dbg!(&events);
-			panic!("assertion failed, see above");
-		}
-
-		assert_eq!(got, expected);
-	}
-
-	fn assert_no_err<Event: AssertHelper, Err: AssertHelper>(&mut self) {
-		self.observe_par_events::<crate::event_wrapper::Event<wire::Res<Event>>>(); // read events to clear them
-		let errs = self
-			.observe_par_events::<crate::event_wrapper::Event<wire::Error<Err>>>()
-			.into_iter()
-			.map(|x| x.into_inner())
-			.collect::<Vec<_>>();
-		if !errs.is_empty() {
-			dbg!(&errs);
-			panic!("assertion failed, see above");
-		}
-	}
-
-	fn update_no_err<Event: AssertHelper, Err: AssertHelper>(&mut self) {
-		self.update();
-		self.assert_no_err::<Event, Err>();
-	}
-
-	fn inspect_state<D: bevy::ecs::query::QueryData>(&mut self, f: impl FnMut(<<D as bevy::ecs::query::QueryData>::ReadOnly as WorldQuery>::Item<'_>)) {
-		self.world_mut().query::<D>().iter(self.world()).for_each(f);
-	}
-
-	fn inspect_res<R: Resource>(&mut self, mut f: impl FnMut(&R)) {
-		f(self.world().get_resource::<R>().expect("resource not found"));
-	}
-}
-
-/// A wrapper type for [`ParManualEventReader`] that implements [`Resource`]
-/// used for observing a parallel [`Event`].
-///
-/// [`ParManualEventReader`]: https://docs.rs/bau/latest/bau/par_events/struct.ParManualEventReader.html
-/// [`Resource`]: https://docs.rs/bevy/latest/bevy/ecs/prelude/trait.Resource.html
-/// [`Event`]: https://docs.rs/bevy/latest/bevy/ecs/event/trait.Event.html
-#[derive(Resource, Debug, Deref, DerefMut)]
-struct ParObserver<E: Event>(ParManualEventReader<E>);
-
-impl<E: Event> Default for ParObserver<E> {
-	fn default() -> Self {
-		Self(ParManualEventReader::default())
 	}
 }
